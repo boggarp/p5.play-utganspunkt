3b9105e4c1e7450be19345df99f4b4304dcc17e3
boggarp <borge.garpestad@vlfk.no>
2022-11-17 09:51:23 +0100

    Moved to npm-packages

--- sketch.js
+++ sketch.js
@@ -1,38 +1,22 @@
-let ball, floor, player;
+//Variabler som skal brukes over lengre tid
+let points = 0
 
 //Setup funksjonen. Ting som skal gjøres en gang i starten
 function setup() {
 	new Canvas(400, 500);
-	world.gravity.y = 10;
 
-  //Lager spiller
-  player = new Sprite()
-  player.y = 50
-  player.x = 50
-
-  //Lager ball
-	ball = new Sprite();
-	ball.diameter = 50;
-	ball.y = 30;
-
-  //Lager gulv
-	floor = new Sprite();
-	floor.y = 495;
-	floor.w = 380;
-	floor.h = 5;
-  floor.collider = 'static';
 }
 
 //Draw funksjonen, ting som skal gjøres for hvert nye bilde som "tegnes".
 //FPS i spill er hvor ofte en slik funksjon klarer å kjøre.
 function draw() {
+  
+  
+  clear(); //Fjerner alt som har blitt tegnet
+}
 
-  //Bevege spiller mot musen
-  player.moveTowards(mouse)
 
-  //Lager nye bokser for hvert bilde som tegnes
-  new Sprite(40,50,10,10)
-  new Sprite(370,50,10,10)
+//Funksjon som kjører viss brukeren klikker på musen
+function mousePressed() {
 
-  clear();
 }
