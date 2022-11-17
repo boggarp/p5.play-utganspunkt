06c3f40ae63e334cc8e84ef04a9434b4b02afebe
boggarp <borge.garpestad@vlfk.no>
2022-11-17 10:04:38 +0100

    Black style, border around canvas

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ let points = 0
 //Setup funksjonen. Ting som skal gjøres en gang i starten
 function setup() {
 	new Canvas(400, 500);
-
 }
 
 //Draw funksjonen, ting som skal gjøres for hvert nye bilde som "tegnes".
 //FPS i spill er hvor ofte en slik funksjon klarer å kjøre.
 function draw() {
+
   
   
   clear(); //Fjerner alt som har blitt tegnet
